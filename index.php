54b6499253b6abc9c5a5b396efb14ce17b2f6e75
matheushmg <matheushmg@hotmail.com>
2018-08-06 14:16:00 -0300

    Criando Usuarios e funções como: Create, Update e Delete

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $app->get('/', function() { // São as Rotas; Função onde os arquivos poderão
 
 $app->get('/admin', function() { 
 
-	User::verifyLogin();
+	User::verifyLogin(); // Verifica se o Usuario está logado ou não
 
 	$page = new PageAdmin();
 
@@ -67,6 +67,101 @@ $app->get('/admin/logout', function() {
 	exit;
 
 });
+
+$app->get('/admin/users', function() { 
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+$app->get('/admin/users/create', function() { 
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+     
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/users/:iduser', function($iduser){ // Modificado para o funcionamento da Edição de Usuarios
+     
+   User::verifyLogin();
+     
+   $user = new User();
+     
+   $user->get((int)$iduser);
+     
+   $page = new PageAdmin();
+     
+   $page ->setTpl("users-update", array(
+   		"user"=>$user->getValues()
+    ));
+     
+});
+
+
+$app->post("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+
+	exit;
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+
+	exit;
+
+});
+
 
 $app->run();
 
--- vendor/matheushmg/php-classes/src/Model/User.php
+++ vendor/matheushmg/php-classes/src/Model/User.php
@@ -66,6 +66,69 @@ class User extends Model{
 
 	}
 
+	public static function listAll(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	public function get($iduser){ // Parte Para o funcionamento da Etapa de Edita Usuarios
+	
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY a.iduser = :iduser", array(":iduser"=>$iduser));
+
+		$data = $results[0];
+
+		$this->setData($data);
+
+	}
+
+	public function save() { // Salva as criações de Usuarios par o banco de dados
+		
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	
+	}
+
+	public function update() {
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	
+	}
+
+	public function delete() {
+		
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+	}
 
 }
 
