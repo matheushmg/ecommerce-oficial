cc085a0fd9c5916e0027d6e5a443bad217d6ac4e
matheushmg <matheushmg@hotmail.com>
2018-08-23 16:00:33 -0300

    Alterando a Senha do Usuario comum

--- rotas/site.php
+++ rotas/site.php
@@ -630,4 +630,75 @@ $app->get("/profile/orders/:idorder", function($idorder){
 
 });
 
+$app->get("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password", [
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+
+});
+
+$app->post("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+		
+		User::setError("Digite a Senha Atual.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+		
+		User::setError("Digite a Nova Senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+		
+		User::setError("Confirme a Nova Senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if ($_POST['current_pass'] === $_POST['new_pass']) {
+
+		User::setError("Nova Senha Terá que ser Diferente da Atual.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	$user = User::getFromSession();
+
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+		User::setError("Senha Inválida.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	User::setSuccess("Senha alterada com Sucesso.");
+	
+	header("Location: /profile/change-password");
+	exit;
+
+});
+
+
 ?>
