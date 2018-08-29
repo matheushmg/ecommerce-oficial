6332c53e330618a27fd641a9ad762ba5e4d8ef05
matheushmg <matheushmg@hotmail.com>
2018-08-29 14:17:55 -0300

    Admin - Usuario - Alterando a Senha do Usuario ADMIN

--- rotas/admin-users.php
+++ rotas/admin-users.php
@@ -5,6 +5,57 @@ use \Matheushmg\PageAdmin;
 use \Matheushmg\Model\User;
 //use \Matheushmg\Model\Category;
 
+$app->get("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+     
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-password", [
+		"user"=>$user->getValues(),
+		"msgError"=>User::getError(),
+		"msgSuccess"=>User::getSuccess()
+	]);
+
+});
+
+$app->post("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	if(!isset($_POST['despassword']) || $_POST['despassword'] === ''){
+		User::setError("Preencha o Campo de senha!");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+	}
+	if(!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm'] === ''){
+		User::setError("Preencha o Campo de confirmação da nova senha!");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+	}
+
+	if($_POST['despassword'] !== $_POST['despassword-confirm'] ){
+		User::setError("Nova senha OU Confimação da senha incorretas/diferentes.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+	}
+
+	$user = new User();
+     
+	$user->get((int)$iduser);
+
+	$user->setPassword(User::getPasswordHash($_POST['despassword']));
+
+	User::setSuccess("Nova senha Alterada Com Sucesso.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+});
+
 $app->get('/admin/users', function() { 
 
 	User::verifyLogin();
